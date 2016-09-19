5d8f146075f9a3c85ad3f1861db16ebe5e946daf
khalid19 <fjroni@gmail.com>
2016-09-19 08:03:51 +0600

    pages
    pages

---
+++ Ajax and jQuery in ASP.NET MVC – chsakell's Blog_files/a_data/a_data_002/a_data/ADTECH.js
@@ -0,0 +1,13 @@
+document.write("<iframe seamless=\"seamless\" style=\"width:0px;height:0px;border:none;position:absolute;\" src=\"//delivery.g.switchadhub.com/adserver/passback.html\"></iframe>\n");
+
+
+document.write('<img src="https://atpixelus.alephd.com/post_adtech?xH3z8opNzJvJ6rtndTDAjCF-nK18PSlu4ycpBzErLGHU9v1.3A88CL-wdnPGDuYl-DmICtrm5Kb0uJ3RK8IRM4Ra8xR0-AHLP.45W65vVJZ-n2d67uFsTqUwWNPhgmH14utTeKRvVnv1dyiM9A3x.grd0eN.muirWbU4RETcwC4OR7EqqTelCcn.e4baxSBt5NsnRMj.AR-PXKZN3.c7tmrZ9JoTlr-vPod8WkHYoNvH1Q5hnIvzF3VukueQqf8SCU9MoqaFk-xRy9UvmLTR4g==" width="1" height="1"  style="display:none;">');
+document.write('<img src="https://rp.gwallet.com/r1/cm/p81r" width="1" height="1" style="display:none;">');
+document.write('<img src="https://idpix.media6degrees.com/orbserv/hbpix?pixId=15194&pcv=50" width="1" height="1" style="display:none;">');
+document.write('<img src="https://x.bidswitch.net/sync?ssp=aol" width="1" height="1" style="display:none;">');
+document.write('<img src="https://s.chango.com/m/aol" width="1" height="1" style="display:none;">');
+document.write('<img src="https://pixel.quantserve.com/pixel/p-NcBg8UA4xqUFp.gif?idmatch=0" width="1" height="1" style="display:none;">');
+document.write('<img src="https://ad.afy11.net/ad?mode=10&sspid=585" width="1" height="1" style="display:none;">');
+document.write('<img src="https://p.rfihub.com/cm?in=1&pub=758&forward=https://ums.adtechus.com/mapuser?providerid=1014;userid={userid}" width="1" height="1" style="display:none;">');
+document.write('<img src="https://ib.adnxs.com/getuid?https://ums.adtechus.com/mapuser?providerid=1001;userid=$UID" width="1" height="1" style="display:none;">');
+document.write('<img src="https://adt.pxl.ace.advertising.com/cfcm.ashx?providerId=1010&extMatch=1&rcode=1" width="1" height="1" style="display:none;">');
